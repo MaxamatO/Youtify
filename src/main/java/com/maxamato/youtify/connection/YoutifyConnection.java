e2f12caf101e3a72847fadc050f9c72d30300ea4
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-08-11 21:57:35 +0200

    Created Connection class for handling tokens

---
+++ src/main/java/com/maxamato/youtify/connection/YoutifyConnection.java
@@ -0,0 +1,59 @@
+package com.maxamato.youtify.connection;
+
+import com.maxamato.youtify.Credentials;
+import okhttp3.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Objects;
+
+@Component
+public class YoutifyConnection {
+
+    private static final String userId = com.maxamato.youtify.Credentials.getUserId();
+    private static final URI redirectUri = URI.create("https://example.com/spotify-redirect");
+    private static final String plName = com.maxamato.youtify.Credentials.getPlName();
+    private static final String refreshToken = Credentials.getRefreshToken();
+    private static final String encoded = Credentials.getEncodedIdSecret();
+
+
+    public String getPlaylists() throws IOException, ParseException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder()
+                .url(String.format("https://api.spotify.com/v1/users/%s/playlists", userId))
+                .addHeader("Authorization", String.format("Bearer %s", obtainAccessToken()))
+                .addHeader("Content-Type", "application/application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public String obtainAccessToken() throws IOException, ParseException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody
+                .create(mediaType,
+                        String.format("grant_type=refresh_token&refresh_token=%s", refreshToken));
+
+        Request request = new Request.Builder()
+                .url("https://accounts.spotify.com/api/token")
+                .method("POST", body)
+                .addHeader("Authorization", String.format("Basic %s", encoded))
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Response response = client.newCall(request).execute();
+        String jsonString = Objects.requireNonNull(response.body()).string();
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject;
+        jsonObject = (JSONObject) parser.parse(jsonString);
+        return jsonObject.get("access_token").toString();
+    }
+
+
+
+}

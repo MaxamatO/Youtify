97bb9b61123350a6a59036965a912240c1c81754
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-08-22 21:31:59 +0200

    Add logic for playlistItems

--- src/main/java/com/maxamato/youtify/Service/YoutifyService.java
+++ src/main/java/com/maxamato/youtify/Service/YoutifyService.java
@@ -1,7 +1,10 @@
 package com.maxamato.youtify.Service;
 
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.PlaylistListResponse;
 import com.maxamato.youtify.Credentials;
-import com.maxamato.youtify.connection.YoutifyConnection;
+import com.maxamato.youtify.connection.SpotifyConnection;
 import lombok.AllArgsConstructor;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -9,17 +12,24 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 
+
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import static com.maxamato.youtify.connection.YoutubeConnection.getService;
 
 
 @Service
 @AllArgsConstructor
 public class YoutifyService {
 
-    private final YoutifyConnection youtifyConnection;
+    private final SpotifyConnection spotifyConnection;
+
+
 
     public String getPlaylists() throws IOException, ParseException {
-        String jsonString = youtifyConnection.getPlaylists();
+        String jsonString = spotifyConnection.getPlaylists();
         JSONObject jsonObject = (JSONObject) new JSONParser().parse(jsonString);
         JSONArray jsonArray = (JSONArray) jsonObject.get("items");
         for (Object o : jsonArray) {
@@ -29,9 +39,18 @@ public class YoutifyService {
                 return record.get("tracks").toString();
             }
         }
+        return spotifyConnection.getPlaylists();
+    }
 
-
-        return youtifyConnection.getPlaylists();
+    public String youtubeAPI()throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+        YouTube youtubeService = getService();
+        // Define and execute the API request
+        YouTube.Playlists.List request = youtubeService.playlists()
+                .list(Collections.singletonList("snippet,contentDetails"));
+        PlaylistListResponse response = request.setMaxResults(25L)
+                .setMine(true)
+                .execute();
+        return response.toString();
     }
 
 

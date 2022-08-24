558cf2f34bcc527d0271ad619f83d988794ddec9
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-08-24 22:23:23 +0200

    *Add* getting videos' names from Youtify playlist at YT

--- src/main/java/com/maxamato/youtify/Service/YoutifyService.java
+++ src/main/java/com/maxamato/youtify/Service/YoutifyService.java
@@ -2,7 +2,7 @@ package com.maxamato.youtify.Service;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.PlaylistListResponse;
+import com.google.api.services.youtube.model.*;
 import com.maxamato.youtify.Credentials;
 import com.maxamato.youtify.connection.SpotifyConnection;
 import lombok.AllArgsConstructor;
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 import static com.maxamato.youtify.connection.YoutubeConnection.getService;
 
@@ -42,16 +44,41 @@ public class YoutifyService {
         return spotifyConnection.getPlaylists();
     }
 
+
     public String youtubeAPI()throws GeneralSecurityException, IOException, GoogleJsonResponseException {
         YouTube youtubeService = getService();
+        // Define and execute the API request
+        YouTube.PlaylistItems.List request = youtubeService.playlistItems()
+                .list(Collections.singletonList("snippet,contentDetails"));
+        PlaylistItemListResponse response = request.setMaxResults(25L)
+                .setPlaylistId(obtainYoutifiesPlaylistId(youtubeService))
+                .execute();
+        List<PlaylistItem> items = response.getItems();
+        for(PlaylistItem playlistItem:items){
+            System.out.println(playlistItem.getSnippet().getTitle());
+        }
+        return response.toString();
+
+    }
+
+    private String obtainYoutifiesPlaylistId(YouTube youtubeService) throws GeneralSecurityException, IOException {
         // Define and execute the API request
         YouTube.Playlists.List request = youtubeService.playlists()
                 .list(Collections.singletonList("snippet,contentDetails"));
         PlaylistListResponse response = request.setMaxResults(25L)
                 .setMine(true)
                 .execute();
-        return response.toString();
+        List<Playlist> items = response.getItems();
+        String playlistName = Credentials.getPlName().toLowerCase();
+        for(Playlist playlist:items){
+            if(playlist.getSnippet().getTitle().toLowerCase().equals(playlistName)){
+                return playlist.getId();
+            }
+        }
+        throw new IllegalStateException(new Exception(String.format("No \"%s\" playlist found", playlistName)));
     }
+
+    // TODO: Implement logic to search for tracks obtained in YoutubeApi() in Spotify and add them to respective playlist.
 
 
 
